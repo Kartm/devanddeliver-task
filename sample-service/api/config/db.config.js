1ee4120f0978b35ddd7738ebc847e9793a9ea2cd
Lukasz Blachnicki (W10) <lblachnicki@gmail.com>
2020-11-04 21:47:09 +0100

    Setup docker compose

--- sample-service/api/config/db.config.js
+++ sample-service/api/config/db.config.js
@@ -1,14 +1,8 @@
 module.exports = {
   HOST: process.env.DATABASE_HOST || "127.0.0.1",
   USER: "root",
-  PASSWORD: "",
+  PASSWORD: "123",
   DB: "CHIRINOS",
   port: 3306,
   dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
 };
--- sample-service/api/server.js
+++ sample-service/api/server.js
@@ -1,14 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 
 const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-
-app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -29,7 +22,7 @@ app.get("/", (req, res) => {
 require("./routes/tutorial.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8123;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
