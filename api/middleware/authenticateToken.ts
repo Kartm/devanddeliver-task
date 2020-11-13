cb3ef4e5e31dd41ab5abf59bf82e9b67a7fa17b3
Łukasz Blachnicki <lblachnicki@gmail.com>
2020-11-13 08:36:04 +0100

    Add missing types to auth middleware

--- api/middleware/authenticateToken.ts
+++ api/middleware/authenticateToken.ts
@@ -1,20 +1,22 @@
-import express, { Request } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export interface RequestWithMetaData extends Request {
+interface MetaData {
   userId: number;
 }
 
+export interface RequestWithMetaData extends Request, MetaData {}
+
 export default function authenticateToken(
   req: RequestWithMetaData,
-  res: express.Response,
-  next
+  res: Response,
+  next: NextFunction
 ) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, "SECRET", (err, decoded) => {
+  jwt.verify(token, "SECRET", (err, decoded: MetaData) => {
     console.log(err);
     if (err) return res.sendStatus(403);
 
